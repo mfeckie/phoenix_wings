e73151cfef32c510855507ad89b487602c174b54
Martin Feckie <mfeckie@gmail.com>
2017-11-22 22:18:10 +0800

    message serialization methods without callbacks

--- lib/phoenix_serializer.dart
+++ lib/phoenix_serializer.dart
@@ -1,14 +1,12 @@
 import 'package:phoenix_wings/phoenix_message.dart';
 
 class PhoenixSerializer {
-  static String encode(PhoenixMessage msg, Function callback) {
+  static String encode(PhoenixMessage msg) {
     return msg.toJSON();
   }
 
-  static decode(String rawPayload, Function callback) {
-//    let [join_ref, ref, topic, event, payload] = JSON.parse(rawPayload)
-//
-//    return callback({join_ref, ref, topic, event, payload})
+  static PhoenixMessage decode(String rawPayload) {
+    return PhoenixMessage.decode(rawPayload);
   }
 }
 
