adbaec97a1463998fdf3238dc20c999e166c829c
Martin Feckie <mfeckie@gmail.com>
2017-11-22 23:22:16 +0800

    Add heartbeat timer and handling

--- lib/phoenix_socket.dart
+++ lib/phoenix_socket.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'package:phoenix_wings/phoenix_message.dart';
 import 'package:phoenix_wings/phoenix_serializer.dart';
 import 'package:phoenix_wings/phoenix_socket_options.dart';
 import 'package:phoenix_wings/phoenix_timer.dart';
@@ -14,20 +15,19 @@ class PhoenixSocket {
   var _encode = PhoenixSerializer.encode;
   var _decode = PhoenixSerializer.decode;
   Timer _heartbeatTimer;
-  int _pendingHeartbeatRef;
+  String _pendingHeartbeatRef;
   PhoenixTimer _reconnectTimer;
   List<Function> _sendBuffer;
   WebSocket conn;
 
   // Optionals
   int timeout = 10000;
-  PhoenixSocketOptions options;
+  PhoenixSocketOptions _options = new PhoenixSocketOptions();
 
   PhoenixSocket(String endpoint, {socketOptions: PhoenixSocketOptions}) {
     if (socketOptions is PhoenixSocketOptions) {
-      options = socketOptions;
-    }
-
+      _options = socketOptions;
+    } 
     buildEndpoint(endpoint);
   }
 
@@ -39,10 +39,11 @@ class PhoenixSocket {
         host: decodedUri.host,
         path: decodedUri.path,
         port: decodedUri.port,
-        queryParameters: options?.params);
+        queryParameters: _options?.params);
   }
 
   get endpoint => _endpoint;
+  get isConnected => conn?.readyState == WebSocket.OPEN;
 
   connect() async {
     if (conn != null) {
@@ -64,21 +65,31 @@ class PhoenixSocket {
 
   onError(Function() callback) => _stateChangeCallbacks.error.add(callback);
 
-  onMessage(Function() callback) => _stateChangeCallbacks.message.add(callback);
+  onMessage(Function(PhoenixMessage) callback) => _stateChangeCallbacks.message.add(callback);
 
-  onConnOpened() async => _stateChangeCallbacks.open.forEach((cb) => cb());
+  onConnOpened() async {
+    _heartbeatTimer = new Timer.periodic(new Duration(milliseconds: _options.heartbeatIntervalMs), sendHeartbeat);
+    _stateChangeCallbacks.open.forEach((cb) => cb());
+  }
   onConnClosed() async => _stateChangeCallbacks.close.forEach((cb) => cb());
+  onErrorOccur() async => _stateChangeCallbacks.error.forEach((cb) => cb());
 
-  onReceive(dynamic msg) {
-    print(msg);
+  onReceive(String rawJSON) {
+    final message = this._decode(rawJSON);
+    print(rawJSON);
+    if (message.ref == _pendingHeartbeatRef) { _pendingHeartbeatRef = null; }
+      // this.channels.filter( channel => channel.isMember(topic, event, payload, join_ref) )
+      //              .forEach( channel => channel.trigger(event, payload, ref, join_ref) )
+    _stateChangeCallbacks.message.forEach((callback) => callback(message));
   }
 
   onConnectionError(error) {
-    print("CONN ERROR");
+    onErrorOccur();
     print(error);
   }
 
   reconnect() async {
+    onConnClosed();
     this.conn = null;
     await new Future<Null>.delayed(new Duration(milliseconds: 100));
     await this.connect();
@@ -89,10 +100,42 @@ class PhoenixSocket {
       this.conn.close(code);
     }
   }
+
+  void sendHeartbeat(Timer timer) {
+    if (conn?.readyState != WebSocket.OPEN) { return; }
+    if (_pendingHeartbeatRef != null) {
+      _pendingHeartbeatRef = null;
+      conn.close(WebSocketStatus.NORMAL_CLOSURE, "Heartbeat timeout");
+      return;
+    }
+    _pendingHeartbeatRef = makeRef();
+
+    
+    push(new PhoenixMessage.heartbeat(_pendingHeartbeatRef));
+  }
+
+  void push(PhoenixMessage msg) {
+    final callback = () {
+      final encoded = this._encode(msg);
+      conn.add(encoded);
+    };
+    if (isConnected) {
+      callback();
+    } else {
+      _sendBuffer.add(callback);
+    }
+  }
+
+  String makeRef() {
+    final newRef = _ref + 1;
+    if (newRef == _ref) { _ref = 0; } else { _ref = newRef; }
+    return "$_ref";
+  }
 }
 
 class StateChangeCallbacks {
-  List<Function()> open, close, error, message;
+  List<Function()> open, close, error;
+  List<Function(PhoenixMessage)> message;
 
   StateChangeCallbacks() {
     this.open = [];
--- test/phoenix_socket_test.dart
+++ test/phoenix_socket_test.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
+import 'dart:convert';
 import 'dart:io';
 
+import 'package:phoenix_wings/phoenix_serializer.dart';
 import 'package:phoenix_wings/phoenix_socket_options.dart';
 import 'package:test/test.dart';
 import 'package:phoenix_wings/phoenix_socket.dart';
@@ -8,6 +10,7 @@ import 'package:phoenix_wings/phoenix_socket.dart';
 class MockServer {
   HttpServer _server;
   WebSocket _socket;
+  var heartbeat = 0;
 
   start() async {
     _server = await HttpServer.bind('localhost', 4000);
@@ -19,7 +22,12 @@ class MockServer {
       if (req.uri.path == '/socket/websocket') {
         _socket = await WebSocketTransformer.upgrade(req);
         _socket.listen((msg) {
-          print(msg);
+          final message = PhoenixSerializer.decode(msg);
+          if (message.event == 'heartbeat') {
+            heartbeat++;
+            final json = JSON.encode([null,"${message.ref}","phoenix","phx_reply",{"status":"ok","response":{}}]);
+            _socket.add(json);
+          }
         }, onError: (msg) {});
       }
     }
@@ -54,7 +62,8 @@ void main() {
 
   test("Accepts query parameters via an options object", () {
     final endpoint = "ws://localhost:4000/socket";
-    final options = new PhoenixSocketOptions(params: {"stuff": "things"});
+    final options = new PhoenixSocketOptions();
+    options.params = {"stuff": "things"};
     final socket = new PhoenixSocket(endpoint, socketOptions: options);
     expect(socket.endpoint.queryParameters, options.params);
   });
@@ -71,4 +80,29 @@ void main() {
 
     expect(callbackCalled, true);
   });
+
+  test("Triggers callbacks on close", () async {
+    final socket = new PhoenixSocket("ws://localhost:4000/socket/websocket");
+
+    var callbackCalled = false;
+    socket.onClose(() { callbackCalled = true; });
+
+    await socket.connect();
+    await server.testDisconnect();
+
+    await new Future<Null>.delayed(new Duration(milliseconds: 10));
+
+    expect(callbackCalled, true);
+  });
+
+  test("Sends heartbeat", () async {
+    final options = new PhoenixSocketOptions();
+    options.heartbeatIntervalMs = 9;
+    final socket = new PhoenixSocket("ws://localhost:4000/socket/websocket", socketOptions: options);
+    await socket.connect();
+
+    await new Future<Null>.delayed(new Duration(milliseconds: 15));
+    expect(server.heartbeat, greaterThan(0));
+  });
+
 }
