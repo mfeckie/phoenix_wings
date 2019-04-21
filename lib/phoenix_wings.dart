1f63d3a194b5071edd33960af2104e24d94dc9a6
Justin Thompson <jthompson.ca@gmail.com>
2019-04-21 16:14:28 -0700

    Include presence in the library exports

--- lib/phoenix_wings.dart
+++ lib/phoenix_wings.dart
@@ -3,6 +3,7 @@ library phoenix_wings;
 export 'src/phoenix_channel.dart';
 export 'src/phoenix_io_connection.dart';
 export 'src/phoenix_message.dart';
+export 'src/phoenix_presence.dart';
 export 'src/phoenix_push.dart';
 export 'src/phoenix_serializer.dart';
 export 'src/phoenix_socket.dart';
